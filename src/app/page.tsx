e07d72fedcfc7272a97bcc44aa915b050c04f4b4
kessie2862 <kayp3477@gmail.com>
2024-12-25 09:12:37 +0000

    Change app name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <main className="flex flex-col gap-6 justify-center items-center min-h-screen text-center max-w-5xl mx-auto">
-      <h1 className="text-5xl font-bold">Invoice App</h1>
+      <h1 className="text-5xl font-bold">BizInvoice</h1>
       <p>
         <Button asChild>
           <Link href="/dashboard">Sign In</Link>
